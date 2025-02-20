5db7055a19627b3d2dbd635bd2ab0ca6fce52bca
Muhammed Fayiz <fayiz149165@gmail.com>
2025-02-20 17:19:46 +0530

    check is fix the cors error

--- src/server.ts
+++ src/server.ts
@@ -249,11 +249,19 @@ cron.schedule("13 18 * * *", () => {
 
 
 
-const corsOptions = {
-  origin: allowedOrigins,
-  optionsSuccessStatus: 200,
+const corsOptions: import("cors").CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
